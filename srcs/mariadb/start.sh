cafe099fb9b806833b4737461088ade4198d2dc8
abalasub <akash04@hotmail.fr>
2025-08-21 18:43:59 +0200

    docker compose fail

--- srcs/mariadb/start.sh
+++ srcs/mariadb/start.sh
@@ -1,19 +1,19 @@
 #!/bin/sh
 
-if [! -d "/var/lib/mysql/mysql"];then
+if [ ! -d "/var/lib/mysql/mysql" ];then
 
-	mysql_install_db --datadir=/var/lib/mysql
+	mariadb-install-db --datadir=/var/lib/mysql
 
-	mysqld --datadir=/var/lib/mysql &
+	mariadbd --datadir=/var/lib/mysql &
 	MYSQL_PID=$!
 
 	until mysqladmin ping -h localhost --silent; do
    	sleep 1
 	done
 
-	mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
-   mysql -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
-   mysql -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+	mariadb -u root -e "CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};"
+   mariadb -u root -e "CREATE USER '${MYSQL_USER}'@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+   mariadb -u root -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
 
 	kill $MYSQL_PID
 	wait $MYSQL_PID
