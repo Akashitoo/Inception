afa3f9f86166110159f178178a7af2522ddf5228
BALASUBRAMANIAM <akash04@hotmail.fr>
2025-08-25 22:25:13 +0200

    healthcheck db

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -3,5 +3,5 @@
 echo 'Config wordpress\n';
 
 wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root	
-
+#wp core install --url="https://abalasub.42.fr" --title="ABALASUB42" --admin_user="abalasub" --admin_password="salut42!" --admin_email="boss@toto.com" --skip-email --allow-root
 exec "$@"
