fa5c1fa105ffcb0c30ea0969e15d7b323e1a2e3e
abalasub <akash04@hotmail.fr>
2025-08-27 19:07:25 +0200

    c bientot la fin? jai braque la drill comme un assassin , jsuis dans la ville jtouche pas la sacem

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -4,4 +4,5 @@ echo 'Config wordpress\n';
 
 wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root	
 #wp core install --url="https://abalasub.42.fr" --title="ABALASUB42" --admin_user="abalasub" --admin_password="salut42!" --admin_email="boss@toto.com" --skip-email --allow-root
+wp core install --url=abalasub.42.fr --title=ABALASUB42 --admin_user=boss --admin_password=boss75018 --admin_email=netflixnvx@gmail.com --allow-root
 exec "$@"
