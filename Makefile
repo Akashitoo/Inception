95ba958ae6da1da9d4a5ea4cbd95f26c269dc515
abalasub <akash04@hotmail.fr>
2025-09-09 17:23:22 +0200

    create 2nd user wp

--- Makefile
+++ Makefile
@@ -2,8 +2,8 @@ all:
 	sudo docker-compose -f srcs/docker-compose.yml up --build
 
 clean:
-	sudo docker-compose -f srcs/docker-compose.yml down
 	sudo docker-compose -f srcs/docker-compose.yml down -v
-	sudo docker system prune -f
-
-re:	clean all
+	
+fclean: clean
+		sudo docker system prune -f
+re: fclean all
--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 
-echo 'Config wordpress\n';
+echo 'Config wordpress';
 
-wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$MYSQL_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root	
+if [ ! -f "/var/www/html/wp-config.php" ];then
+	wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root	
+fi 
 #wp core install --url="https://abalasub.42.fr" --title="ABALASUB42" --admin_user="abalasub" --admin_password="salut42!" --admin_email="boss@toto.com" --skip-email --allow-root
-wp core install --url=abalasub.42.fr --title=ABALASUB42 --admin_user=boss --admin_password=boss75018 --admin_email=netflixnvx@gmail.com --allow-root
+if ! wp core is-installed --allow-root; then
+	wp core install --url=abalasub.42.fr --title=ABALASUB42 --admin_user=boss --admin_password=boss75018 --admin_email=netflixnvx@gmail.com --skip-email --allow-root 
+fi
+
+if ! wp user exists modo --allow-root; then
+	wp user create modo modo@gmail.com --role=author --user_pass=modo75018 --allow-root
+fi
 exec "$@"
