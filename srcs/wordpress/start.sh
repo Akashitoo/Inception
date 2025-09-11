a02f8851fc2dd413a7646d0f950dddfa89a5bed8
abalasub <akash04@hotmail.fr>
2025-09-11 15:36:23 +0200

    certif ssl generate

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -4,8 +4,7 @@ echo 'Config wordpress';
 
 if [ ! -f "/var/www/html/wp-config.php" ];then
 	wp config create --dbname="$WORDPRESS_DB_NAME" --dbuser="$WORDPRESS_DB_USER" --dbpass="$WORDPRESS_DB_PASSWORD" --dbhost="$WORDPRESS_DB_HOST" --allow-root	
-fi 
-#wp core install --url="https://abalasub.42.fr" --title="ABALASUB42" --admin_user="abalasub" --admin_password="salut42!" --admin_email="boss@toto.com" --skip-email --allow-root
+fi
 if ! wp core is-installed --allow-root; then
 	wp core install --url=abalasub.42.fr --title=ABALASUB42 --admin_user=boss --admin_password=boss75018 --admin_email=netflixnvx@gmail.com --skip-email --allow-root 
 fi
